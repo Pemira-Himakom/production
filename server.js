f3e41a509a41fe6c95c2d967f5224f0cf152ed26
Antonius Teddy Kurniawan <anamalocaris@Antoniuss-MacBook-Pro.local>
2022-11-09 00:03:47 +0700

    minor fix on server.js

--- server.js
+++ server.js
@@ -34,6 +34,13 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URI);
 }
 
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  })
+}
+
 app.listen(process.env.PORT || 3001, function () {
   console.log("Server started at port 3001");
 });
