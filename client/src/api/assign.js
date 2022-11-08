9d52114b80bd4a9c9f78ea9af13f3b515cbaead3
JonathanEdmund <jonathan.edmund@gmail.com>
2022-11-09 00:03:33 +0700

    fix api fetch

--- client/src/api/assign.js
+++ client/src/api/assign.js
@@ -1,8 +1,8 @@
-import Cookies from "universal-cookie";
-const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
+// import Cookies from "universal-cookie";
+// const BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
 
-export const assign = async input => {
-    const response = await fetch(`${BASE_URL}/api/admin/assign`, {
+export const assign = async (input) => {
+  const response = await fetch(`/api/admin/assign`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
