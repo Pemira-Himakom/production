6522d78f22ac2c74aca136927ef663cca881aad1
JonathanEdmund <jonathan.edmund@gmail.com>
2022-11-09 01:44:27 +0700

    switch from path.resolve to path.join

--- server.js
+++ server.js
@@ -36,8 +36,9 @@ async function main() {
 
 if (process.env.NODE_ENV === "production"){
   app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    // res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname,"/client/build/index.html"))
   })
 }
 
