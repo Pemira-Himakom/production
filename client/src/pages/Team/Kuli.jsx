f9b3e4bab85838abc90d9183b65301f13b3339fa
sickopam <sickopam@gmail.com>
2022-11-17 15:53:23 +0700

    finished teampage unresponsive (no design)

---
+++ client/src/pages/Team/Kuli.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {ReactComponent as Arrow} from '../../img/team/arrow.svg'
+import Pm from '../../img/team/pm.webp'
+import Be from '../../img/team/be.webp'
+import Uiux from '../../img/team/uiux.webp'
+
+export default function Kuli() {
+  return (
+    <div className='h-screen bg-[#21201D] px-8 py-16 font-prata space-y-10'>
+        <div className='flex justify-evenly px-[32rem]'>
+            <Arrow/>
+            <h1 className='prim-color text-3xl'>Crew</h1>
+            <Arrow/>
+        </div>
+
+        <div className='teambox'>
+            <div className='flex flex-row justify-evenly h-full text-[#ffff] text-lg'>
+                {/* <div className='border-r-[3px] border-[#D4A976]'> */}
+                <div className='w-[20%] h-fit space-y-5 mt-8'>
+                    <img src={Pm} alt='Pm'/>
+                    <h1>Project Manager</h1>
+                </div>
+                <div className='h-full border-[2px] border-[#D4A976]'/>
+                
+                <div className='w-[20%] h-fit self-end space-y-5 mb-8'>
+                    <h1>UI/UX</h1>
+                    <img src={Uiux} alt='Uiux'/>
+                </div>
+                <div className='h-full border-[2px] border-[#D4A976]'/>
+
+                <div className='w-[20%] h-fit space-y-5 mt-8'>
+                    <img src={Pm} alt='Fe'/>
+                    <h1>Frontend</h1>
+                </div>
+                <div className='h-full border-[2px] border-[#D4A976]'/>
+
+                <div className='w-[20%] h-fit self-end space-y-5 mb-8'>
+                    <h1>Backend</h1>
+                    <img src={Be} alt='Be'/>
+                </div>
+                
+                {/* </div> */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+const tim = [
+    
+]
--- client/src/pages/Team/Webteam.jsx
+++ client/src/pages/Team/Webteam.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import logocircle  from '../../img/team/himacircle.svg'
+import Kuli from './Kuli'
 
 export default function Webteam() {
   return (
-    <div className='webteam h-screen'>
-      <h1 className='text-5xl lg:text-8xl font-prata prim-color pl-48 pt-48 relative top-[50px]'>Meet our <br></br>Website Team</h1>
-      <div className='w-[75%] h-[37%] bg-[#18150E]'>
-        <img className='absolute left-[65%] w-[20%]' src={logocircle}></img>
-      </div>
-    </div>
+    <>
+        <div className='webteam h-screen'>
+        <h1 className='text-5xl lg:text-8xl font-prata prim-color pl-48 pt-48 relative top-[50px]'>Meet our <br></br>Website Team</h1>
+        <div className='w-[75%] h-[37%] bg-[#18150E]'>
+            <img className='absolute left-[65%] w-[20%]' src={logocircle}></img>
+        </div>
+        </div>
+        <Kuli/>
+    </>
   )
 }
